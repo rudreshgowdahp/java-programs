b1958b400f688a7996d850212f19778f272fe323
rudresh <rudreshgowdahp@gmail.com>
2026-03-31 17:06:26 +0530

    Method overLoading

--- overriding and overloading/src/com/thinksworks/overriding/external/Person.java
+++ overriding and overloading/src/com/thinksworks/overriding/external/Person.java
@@ -4,6 +4,9 @@ public class Person {
      public void role(){
          System.out.println("Person has a role");
      }
+     public void role2(){
+         System.out.println("Rudresh");
+     }
 
 
 
