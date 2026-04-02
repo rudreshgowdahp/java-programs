14e7710249ce2792ebbbd8ba001d5b5ff174fe49
rudresh <rudreshgowdahp@gmail.com>
2026-04-02 21:52:38 +0530

    Updating one line

---
+++ types of inheritance/src/com/thinksworks/types/internal/Bike.java
@@ -0,0 +1,9 @@
+package com.thinksworks.types.internal;
+
+import com.thinksworks.types.external.Parent;
+
+public class Bike extends Parent {
+    public void instanceMethod(){
+        System.out.println("Instance method in class");
+    }
+}
---
+++ types of inheritance/src/com/thinksworks/types/internal/Truck.java
@@ -0,0 +1,10 @@
+package com.thinksworks.types.internal;
+
+import com.thinksworks.types.external.Parent;
+
+public class Truck extends Parent {
+    public void instanceMethod(){
+        System.out.println("Instance");
+
+    }
+}
