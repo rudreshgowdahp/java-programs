88e768a0260aab1f3a334a48ee18a26ba2c1d3d4
rudresh <rudreshgowdahp@gmail.com>
2026-04-02 21:45:32 +0530

    Upcasting and Downcasting in project

--- types of inheritance/src/com/thinksworks/types/internal/Child.java
+++ types of inheritance/src/com/thinksworks/types/internal/Child.java
@@ -6,6 +6,8 @@ public class Child  extends Parent {
      public int id;
      public  void method(){
         System.out.println("method from child class");
+        System.out.println("Rudresh");
+        System.out.println("Sathish");
 
     }
 
