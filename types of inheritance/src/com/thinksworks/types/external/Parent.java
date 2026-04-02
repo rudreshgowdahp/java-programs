0de043dee2f12615ea048ba64ca6e73148a6f4bb
rudresh <rudreshgowdahp@gmail.com>
2026-04-02 21:36:49 +0530

    Upcasting and Downcasting changes

--- types of inheritance/src/com/thinksworks/types/external/Parent.java
+++ types of inheritance/src/com/thinksworks/types/external/Parent.java
@@ -4,6 +4,8 @@ public class Parent {
      public String name;
      public  void method(){
         System.out.println("Method from parent class");
-
-    }
+     }
+     public void instanceMethod(){
+         System.out.println("Instance method");
+     }
 }
