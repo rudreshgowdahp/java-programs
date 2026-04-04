ecdf88ec9679512c9fb67c43b5baf97baf50363d
rudresh <rudreshgowdahp@gmail.com>
2026-04-04 16:15:09 +0530

    creating methods

---
+++ types of inheritance/src/com/thinksworks/types/internal/Child2.java
@@ -0,0 +1,10 @@
+package com.thinksworks.types.internal;
+
+import com.thinksworks.types.external.Parent;
+
+public class Child2 extends Parent {
+    @Override
+    public void method() {
+        System.out.println("method from child2");
+    }
+}
