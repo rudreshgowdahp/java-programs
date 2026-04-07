99c37410bd044c856cf5b9892812affafa463d80
rudresh <rudreshgowdahp@gmail.com>
2026-04-07 22:39:03 +0530

    creating methods invoking in mainrunner

--- types of inheritance/src/com/thinksworks/types/internal/Child2.java
+++ types of inheritance/src/com/thinksworks/types/internal/Child2.java
@@ -2,9 +2,4 @@ package com.thinksworks.types.internal;
 
 import com.thinksworks.types.external.Parent;
 
-public class Child2 extends Parent {
-    @Override
-    public void method() {
-        System.out.println("method from child2");
-    }
-}
+
