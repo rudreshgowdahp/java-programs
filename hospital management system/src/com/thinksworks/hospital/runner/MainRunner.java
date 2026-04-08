bae64962725ef541aff45f9b055170edf18c65e0
rudresh <rudreshgowdahp@gmail.com>
2026-04-08 15:51:15 +0530

    create patient class

--- hospital management system/src/com/thinksworks/hospital/runner/MainRunner.java
+++ hospital management system/src/com/thinksworks/hospital/runner/MainRunner.java
@@ -1,7 +1,10 @@
 package com.thinksworks.hospital.runner;
 
+import com.thinksworks.hospital.dto.Patient;
+
 public class MainRunner {
     public static void main(String[] args) {
+        Patient patient = new Patient(1,"Rudra",12);
 
     }
 }
