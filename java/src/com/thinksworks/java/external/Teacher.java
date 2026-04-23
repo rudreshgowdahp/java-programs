6f6c83e8d206ac743e7c39382fa97b3eac07172f
rudresh <rudreshgowdahp@gmail.com>
2026-04-23 22:13:08 +0530

    create one project

---
+++ java/src/com/thinksworks/java/external/Teacher.java
@@ -0,0 +1,8 @@
+package com.thinksworks.java.external;
+
+public class Teacher  extends Person{
+    @Override
+    public void personDetails() {
+        System.out.println("Details in child class");
+    }
+}
