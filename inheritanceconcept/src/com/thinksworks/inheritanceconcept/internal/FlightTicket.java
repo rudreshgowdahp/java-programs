6cfa51b6f3a5d7128f7fbfaa472b29eefa6527ff
rudresh <rudreshgowdahp@gmail.com>
2026-04-23 22:09:49 +0530

    updating banking Project

--- inheritanceconcept/src/com/thinksworks/inheritanceconcept/internal/FlightTicket.java
+++ inheritanceconcept/src/com/thinksworks/inheritanceconcept/internal/FlightTicket.java
@@ -3,7 +3,7 @@ package com.thinksworks.inheritanceconcept.internal;
 import com.thinksworks.inheritanceconcept.external.Ticket;
 
 public class FlightTicket extends Ticket {
-    public FlightTicket(int  , int price, String ticketDate, int size) {
+    public FlightTicket( int price, String ticketDate, int size) {
         super(ticket, price, ticketDate, size);
     }
 }
