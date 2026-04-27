50eb7c087953e1d14b0444e97c20d3f34680afd6
rudresh <rudreshgowdahp@gmail.com>
2026-04-27 19:06:06 +0530

    completed Device management system

---
+++ Object/src/com/thinksworks/object/runner/MainRunner.java
@@ -0,0 +1,11 @@
+package com.thinksworks.object.runner;
+
+import com.thinksworks.object.external.Employee;
+
+public class MainRunner {
+    public static void main(String[] args) {
+        Employee  employee = new Employee("Rudresh",14,3,"25/10/2003",1,"Laggere",true);
+        Employee employee1 = new Employee("Rudresh",14,3,"25/10/2003",1,"Laggere",true);
+    }
+
+}
