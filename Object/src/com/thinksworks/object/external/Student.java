b49bfac9a1ef4783508721bbf70863ffb6de9b32
rudresh <rudreshgowdahp@gmail.com>
2026-04-28 20:58:20 +0530

    checking arguments are same

---
+++ Object/src/com/thinksworks/object/external/Student.java
@@ -0,0 +1,11 @@
+package com.thinksworks.object.external;
+
+public class Student {
+        int id;
+        String name;
+        int age;
+        String course;
+        double marks;
+        String email;
+        long phone;
+}
