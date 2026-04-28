214d86dd8e5e74b096d24d269d3191b2e0c9b6ad
rudresh <rudreshgowdahp@gmail.com>
2026-04-28 13:09:47 +0530

    checking arguments are same

--- Object/src/com/thinksworks/object/runner/MainRunner.java
+++ Object/src/com/thinksworks/object/runner/MainRunner.java
@@ -5,7 +5,13 @@ import com.thinksworks.object.external.Employee;
 public class MainRunner {
     public static void main(String[] args) {
         Employee  employee = new Employee("Rudresh",14,3,"25/10/2003",1,"Laggere",true);
-        Employee employee1 = new Employee("Rudresh",14,3,"25/10/2003",1,"Laggere",true);
+        Employee employee1 = new Employee("Rudresh",1,3,"25/10/2003",1,"Laggere",true);
+        if (employee.equals(employee1)){
+            System.out.println("same");
+        }
+        else{
+            System.out.println("nope");
+        }
     }
 
 }
