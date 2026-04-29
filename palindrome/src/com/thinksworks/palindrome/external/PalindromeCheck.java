a13eef0b20da863a0c70ced52d9490d41f1168db
rudresh <rudreshgowdahp@gmail.com>
2026-04-29 18:00:58 +0530

    palindrome checking

---
+++ palindrome/src/com/thinksworks/palindrome/external/PalindromeCheck.java
@@ -0,0 +1,17 @@
+package com.thinksworks.palindrome.external;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String name = "madam";
+        String reversed = "";
+        for (int i = name.length()-1; i >= 0; i--) {
+            reversed = reversed+ name.charAt(i);
+        }
+        if (name.equals(reversed)){
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("not palindrome");
+        }
+    }
+}
