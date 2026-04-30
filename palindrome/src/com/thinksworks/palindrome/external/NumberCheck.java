7a6be710208a3d6383d87ae1bcbb07d81f85fd8b
rudresh <rudreshgowdahp@gmail.com>
2026-04-30 21:10:26 +0530

    even or odd checking

---
+++ palindrome/src/com/thinksworks/palindrome/external/NumberCheck.java
@@ -0,0 +1,12 @@
+package com.thinksworks.palindrome.external;
+
+public class NumberCheck {
+    public static void main(String[] args) {
+        int number = 2;
+        if (number %2 == 0 ){
+            System.out.println("even");
+        }else {
+            System.out.println("odd");
+        }
+    }
+}
--- palindrome/src/com/thinksworks/palindrome/external/PalindromeCheck.java
+++ palindrome/src/com/thinksworks/palindrome/external/PalindromeCheck.java
@@ -2,8 +2,24 @@ package com.thinksworks.palindrome.external;
 
 public class PalindromeCheck {
     public static void main(String[] args) {
-        String name = "madam";
+        String name = "";
         String reversed = "";
+
+        for (int i = name.length()-1; i >= 0; i--) {
+            reversed = reversed+ name.charAt(i);
+        }
+        if (name.equals(reversed)){
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("not palindrome");
+        }
+    }
+
+    void checkPalidrome(){
+        String name = "";
+        String reversed = "";
+
         for (int i = name.length()-1; i >= 0; i--) {
             reversed = reversed+ name.charAt(i);
         }
